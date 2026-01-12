01776bbd6eeb91e2c9a25ad42d831725aaefb902
gelexxi <schew4uk98@gmail.com>
2026-01-12 20:58:24 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -3,23 +3,24 @@ from os import remove, rename, makedirs, path
 
 def move_file(command: str) -> None:
     command_path = command.split()
-    if len(command_path) != 3 or "mv" not in command_path:
+    if len(command_path) != 3 or "mv" != command_path[0]:
         return
     if "/" not in command_path[2]:
         rename(command_path[1], command_path[2])
         return
 
-    directory = path.dirname(command_path[2])
+    source_file, destination_file = command_path[1], command_path[2]
+
+    if destination_file.endswith("/"):
+        file_name = path.basename(source_file)
+        destination_file = path.join(destination_file, file_name)
+
+    directory = path.dirname(destination_file)
 
     if directory:
         makedirs(directory, exist_ok=True)
 
-    source_file, destination_file = command_path[1], command_path[2]
-
-    try:
-        with (open(source_file, "r") as file_in,
-              open(destination_file, "w") as file_out):
-            file_out.write(file_in.read())
-        remove(source_file)
-    except Exception:
-        pass
+    with (open(source_file, "r") as file_in,
+          open(destination_file, "w") as file_out):
+        file_out.write(file_in.read())
+    remove(source_file)
